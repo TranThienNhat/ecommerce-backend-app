2cb5190a945ec3021355a85c8124d49d97619c29
TranThienNhat <168163478+TranThienNhat@users.noreply.github.com>
2025-03-30 19:43:32 +0700

    chore: setup MySQL database and Swagger config

---
+++ src/main/java/com/nhat/ecommerce_backend/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.nhat.ecommerce_backend.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("E-Commerce Backend API")
+                        .version("1.0")
+                        .description("API documentation for E-Commerce project"));
+    }
+}
