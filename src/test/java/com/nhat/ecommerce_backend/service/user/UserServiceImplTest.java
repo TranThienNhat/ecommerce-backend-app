ceeb2099e6f6b5561ce5da0fb376abbf7f869ed4
TranThienNhat <168163478+TranThienNhat@users.noreply.github.com>
2025-04-11 00:34:28 +0700

    test(user): move UserServiceImplTest to user package for better structure

--- src/test/java/com/nhat/ecommerce_backend/service/user/UserServiceImplTest.java
+++ src/test/java/com/nhat/ecommerce_backend/service/user/UserServiceImplTest.java
@@ -1,10 +1,10 @@
-package com.nhat.ecommerce_backend.service;
+package com.nhat.ecommerce_backend.service.user;
 
 import com.nhat.ecommerce_backend.dto.user.request.RegisterRequest;
 import com.nhat.ecommerce_backend.entity.User;
 import com.nhat.ecommerce_backend.exception.BusinessException;
 import com.nhat.ecommerce_backend.repository.UserRepository;
-import com.nhat.ecommerce_backend.service.user.UserServiceImpl;
+import com.nhat.ecommerce_backend.service.CartService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
