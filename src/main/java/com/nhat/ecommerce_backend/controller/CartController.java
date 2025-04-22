58ea3095892a25706f58349e18160375730d4ae1
TranThienNhat <tranthiennhat20041@gmail.com>
2025-04-23 01:08:18 +0700

    refactor: simplify CartController by removing userId from getCart endpoint

--- src/main/java/com/nhat/ecommerce_backend/controller/CartController.java
+++ src/main/java/com/nhat/ecommerce_backend/controller/CartController.java
@@ -1,11 +1,14 @@
 package com.nhat.ecommerce_backend.controller;
 
+import com.nhat.ecommerce_backend.dto.cart.CartItemResponse;
 import com.nhat.ecommerce_backend.entity.Cart;
 import com.nhat.ecommerce_backend.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/cart")
@@ -14,12 +17,10 @@ public class CartController {
 
     private final CartService cartService;
 
-    @PreAuthorize("hasRole('ADMIN')")
-    @GetMapping("/user/{userId}")
-    public ResponseEntity<Cart> getCartForUser(@PathVariable Long userId) {
-        return cartService.getCartForUser(userId)
-                .map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
+    @GetMapping
+    public ResponseEntity<List<CartItemResponse>> getCartItem() {
+        List<CartItemResponse> cartItems = cartService.getCartItems();
+        return ResponseEntity.ok(cartItems);
     }
 }
 
