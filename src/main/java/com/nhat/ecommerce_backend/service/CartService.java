ec3ce3f7cc048757631c8561cf6b6a45f552ebae
TranThienNhat <tranthiennhat20041@gmail.com>
2025-04-23 01:08:37 +0700

    refactor: move cart item listing logic to CartService for cleaner responsibility

--- src/main/java/com/nhat/ecommerce_backend/service/CartService.java
+++ src/main/java/com/nhat/ecommerce_backend/service/CartService.java
@@ -1,24 +1,31 @@
 package com.nhat.ecommerce_backend.service;
 
+import com.nhat.ecommerce_backend.dto.cart.CartItemResponse;
 import com.nhat.ecommerce_backend.entity.Cart;
+import com.nhat.ecommerce_backend.entity.CartItem;
+import com.nhat.ecommerce_backend.entity.Product;
 import com.nhat.ecommerce_backend.entity.User;
+import com.nhat.ecommerce_backend.exception.BusinessException;
 import com.nhat.ecommerce_backend.repository.CartRepository;
+import com.nhat.ecommerce_backend.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class CartService {
 
     private final CartRepository cartRepository;
+    private final CartItemService cartItemService;
 
-    public Optional<Cart> getCartForUser(Long userId) {
-        return cartRepository.findByUserId(userId);
-    }
+    private final UserService userService;
 
     public void createCart(User user) {
 
@@ -31,13 +38,29 @@ public class CartService {
 
     public Cart getCartById(UUID cartId) {
         return cartRepository.findById(cartId)
-                .orElseThrow(()-> new RuntimeException("Không tìm thấy cart !"));
+                .orElseThrow(()-> new BusinessException("Cart not found"));
     }
 
-    public Cart getCartByUserId(Long userId) {
-        return cartRepository.findByUserId(userId)
-                .orElseThrow(() -> new RuntimeException("Không tìm thấy giỏ hàng của user"));
-    }
+    public List<CartItemResponse> getCartItems() {
+        Long user = userService.getProfile().getId();
+        Cart cart = cartRepository.findByUserId(user)
+                .orElseThrow(() -> new BusinessException("Cart not found"));
+        List<CartItem> cartItems = cartItemService.getCartItemByCart(cart.getId());
 
+        return cartItems.stream().map(item -> {
+            Product product = item.getProduct();
+
+            CartItemResponse cartItemResponse = new CartItemResponse();
+            cartItemResponse.setId(item.getId());
+            cartItemResponse.setProductId(item.getProduct().getId());
+            cartItemResponse.setProductName(item.getProduct().getName());
+            cartItemResponse.setProductImage(item.getProduct().getImageUrl());
+            cartItemResponse.setPrice(product.getPrice());
+            cartItemResponse.setQuantity(item.getQuantity());
+            cartItemResponse.setTotal(product.getPrice().multiply(BigDecimal.valueOf(item.getQuantity())));
+
+            return cartItemResponse;
+        }).collect(Collectors.toList());
+    }
 }
 
