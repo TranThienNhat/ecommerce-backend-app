5a14dc806f22a2ef31fc9d760d9484c5ca5eaa65
TranThienNhat <tranthiennhat20041@gmail.com>
2025-04-28 13:32:51 +0700

    feat(auth-service): Add refresh token creation and validation, update method names

--- src/main/java/com/nhat/ecommerce_backend/config/JwtUtil.java
+++ src/main/java/com/nhat/ecommerce_backend/config/JwtUtil.java
@@ -11,11 +11,13 @@ import java.util.Date;
 @Component
 public class JwtUtil {
 
-    private static final String SECRET_KEY = "anhyeuemnhieunhieunhieunhieulam123456";
+    private static final String ACCESS_SECRET_KEY = "anhyeuemnhieunhieunhieunhieulam123456";
+    private static final String REFRESH_SECRET_KEY = "emyeuanhnhieunhieunhieunhieulam654321";
 
-    private final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    private final Key refreshKey = Keys.hmacShaKeyFor(REFRESH_SECRET_KEY.getBytes());
+    private final Key accessKey = Keys.hmacShaKeyFor(ACCESS_SECRET_KEY.getBytes());
 
-    public String generateToken(UserDetails userDetails) {
+    public String generateAccessToken(UserDetails userDetails) {
         return Jwts.builder()
                 .setSubject(userDetails.getUsername())
                 .claim("role",userDetails.getAuthorities().stream()
@@ -23,25 +25,54 @@ public class JwtUtil {
                         .toList())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
-                .signWith(key, SignatureAlgorithm.HS256)
+                .signWith(accessKey, SignatureAlgorithm.HS256)
                 .compact();
     }
 
-    public String extractEmail(String token) {return parseToken(token).getBody().getSubject();}
+    public String generateRefreshToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000L * 60 * 60 * 24 * 7)) // 7 ngày
+                .signWith(refreshKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+    public String extractEmailByAccessToken(String token) {return parseAccessToken(token).getBody().getSubject();}
 
     public boolean isTokenValid(String token) {
         try {
-            parseToken(token);
+            parseAccessToken(token);
             return true;
         } catch (JwtException e) {
             return false;
         }
     }
 
-    private Jws<Claims> parseToken (String token) {
+    public String extractEmailByRefreshToken(String token) {return parseRefreshToken(token).getBody().getSubject();}
+
+
+    private Jws<Claims> parseAccessToken (String token) {
         return Jwts.parserBuilder()
-                .setSigningKey(key)
+                .setSigningKey(accessKey)
                 .build()
                 .parseClaimsJws(token);
+    }
+
+    private Jws<Claims> parseRefreshToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(refreshKey)
+                .build()
+                .parseClaimsJws(token);
+    }
+
+    public boolean isRefreshTokenValid(String token) {
+        try {
+            parseRefreshToken(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
     }
 }
