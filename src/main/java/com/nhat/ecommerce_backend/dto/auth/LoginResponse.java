5b7485760d60c55e22dd5b049245a3d92959383d
TranThienNhat <tranthiennhat20041@gmail.com>
2025-04-28 13:33:56 +0700

    fix(auth-service): Update LoginResponse to return both access and refresh tokens

--- src/main/java/com/nhat/ecommerce_backend/dto/auth/LoginResponse.java
+++ src/main/java/com/nhat/ecommerce_backend/dto/auth/LoginResponse.java
@@ -1,10 +1,16 @@
 package com.nhat.ecommerce_backend.dto.auth;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class LoginResponse {
-    private String token;
+
+    private String refreshToken;
+    private String accessToken;
 }
