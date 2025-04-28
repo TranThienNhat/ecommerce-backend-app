5aea54a1a5a8c886d93f791e2f7567a24410a1f3
TranThienNhat <tranthiennhat20041@gmail.com>
2025-04-28 13:28:01 +0700

    fix(auth-service): Update login logic to support refresh token generation

--- src/main/java/com/nhat/ecommerce_backend/service/auth/AuthenticationServiceImpl.java
+++ src/main/java/com/nhat/ecommerce_backend/service/auth/AuthenticationServiceImpl.java
@@ -3,6 +3,7 @@ package com.nhat.ecommerce_backend.service.auth;
 import com.nhat.ecommerce_backend.config.JwtUtil;
 import com.nhat.ecommerce_backend.dto.auth.LoginRequest;
 import com.nhat.ecommerce_backend.dto.auth.LoginResponse;
+import com.nhat.ecommerce_backend.service.refreshtoken.RefreshTokenService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -19,8 +20,8 @@ public class AuthenticationServiceImpl implements AuthenticationService{
     private final AuthenticationManager authenticationManager;
     private final UserDetailsService userDetailsService;
     private final JwtUtil jwtUtil;
+    private final RefreshTokenService refreshTokenService;
 
-    @Override
     public LoginResponse login(LoginRequest request) {
         log.info("Attempting login for email: {}", request.getEmail());
 
@@ -28,9 +29,13 @@ public class AuthenticationServiceImpl implements AuthenticationService{
                 new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
 
         UserDetails userDetails = userDetailsService.loadUserByUsername(request.getEmail());
-        var token = jwtUtil.generateToken(userDetails);
+        String accessToken = jwtUtil.generateAccessToken(userDetails);
+        String refreshToken = refreshTokenService.createRefreshToken(userDetails);
 
         log.info("Login successful for email: {}", request.getEmail());
-        return new LoginResponse(token);
+        return LoginResponse.builder()
+                .refreshToken(refreshToken)
+                .accessToken(accessToken)
+                .build();
     }
 }
