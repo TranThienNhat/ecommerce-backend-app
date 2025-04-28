d94a1c5006ab82e431710b3d433c085f2a5133bb
TranThienNhat <tranthiennhat20041@gmail.com>
2025-04-28 13:34:24 +0700

    feat(entity): Add RefreshToken entity for storing refresh token data

---
+++ src/main/java/com/nhat/ecommerce_backend/entity/RefreshToken.java
@@ -0,0 +1,25 @@
+package com.nhat.ecommerce_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String refreshToken;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+}
