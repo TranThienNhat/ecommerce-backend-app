dc6cae32d2ad143ebbf97ca345a41b298489ec6b
TranThienNhat <tranthiennhat20041@gmail.com>
2025-04-28 13:28:56 +0700

    fix(auth-service-test): Update tests to verify access and refresh token generation

--- src/test/java/com/nhat/ecommerce_backend/service/auth/AuthenticationServiceImplTest.java
+++ src/test/java/com/nhat/ecommerce_backend/service/auth/AuthenticationServiceImplTest.java
@@ -3,6 +3,7 @@ package com.nhat.ecommerce_backend.service.auth;
 import com.nhat.ecommerce_backend.config.JwtUtil;
 import com.nhat.ecommerce_backend.dto.auth.LoginRequest;
 import com.nhat.ecommerce_backend.dto.auth.LoginResponse;
+import com.nhat.ecommerce_backend.service.refreshtoken.RefreshTokenService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -28,6 +29,9 @@ class AuthenticationServiceImplTest {
     @Mock
     private JwtUtil jwtUtil;
 
+    @Mock
+    private RefreshTokenService refreshTokenService;
+
     @InjectMocks
     private AuthenticationServiceImpl authenticationService;
 
@@ -38,12 +42,14 @@ class AuthenticationServiceImplTest {
 
         UserDetails mockUserDetails = mock(UserDetails.class);
         when(userDetailsService.loadUserByUsername(loginRequest.getEmail())).thenReturn(mockUserDetails);
-        when(jwtUtil.generateToken(mockUserDetails)).thenReturn("mocked-jwt-token");
+        when(jwtUtil.generateAccessToken(mockUserDetails)).thenReturn("mocked-jwt-access-token");
+        when(refreshTokenService.createRefreshToken(mockUserDetails)).thenReturn("mocked-jwt-refresh-token");
 
         LoginResponse response = authenticationService.login(loginRequest);
 
         assertNotNull(response);
-        assertEquals("mocked-jwt-token", response.getToken());
+        assertEquals("mocked-jwt-token", response.getAccessToken());
+        assertEquals("mocked-jwt-access-token", response.getRefreshToken());
 
         verify(authenticationManager).authenticate(
                 new UsernamePasswordAuthenticationToken(loginRequest.getEmail(),loginRequest.getPassword()));
