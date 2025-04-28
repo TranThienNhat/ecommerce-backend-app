c96a67525cb90f690073a8af076f1c1207e37cd5
TranThienNhat <tranthiennhat20041@gmail.com>
2025-04-28 13:23:25 +0700

    feat(auth-controller): Add refreshToken and logout APIs to AuthController

--- src/main/java/com/nhat/ecommerce_backend/controller/AuthController.java
+++ src/main/java/com/nhat/ecommerce_backend/controller/AuthController.java
@@ -2,8 +2,11 @@ package com.nhat.ecommerce_backend.controller;
 
 import com.nhat.ecommerce_backend.dto.auth.LoginResponse;
 import com.nhat.ecommerce_backend.dto.auth.LoginRequest;
+import com.nhat.ecommerce_backend.dto.refreshtoken.RefreshTokenRequest;
+import com.nhat.ecommerce_backend.dto.refreshtoken.RefreshTokenResponse;
 import com.nhat.ecommerce_backend.dto.user.RegisterRequest;
 import com.nhat.ecommerce_backend.service.auth.AuthenticationService;
+import com.nhat.ecommerce_backend.service.refreshtoken.RefreshTokenService;
 import com.nhat.ecommerce_backend.service.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -20,9 +23,10 @@ public class AuthController {
 
     private final UserService userService;
     private final AuthenticationService authenticationservice;
+    private final RefreshTokenService refreshTokenService;
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody @Valid RegisterRequest request) {
+    public ResponseEntity<String> register(@RequestBody @Valid RegisterRequest request) {
         userService.registerUser(request);
         return ResponseEntity.ok("Registration successful!");
     }
@@ -31,6 +35,18 @@ public class AuthController {
     public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest request) {
         var token = authenticationservice.login(request);
         return ResponseEntity.ok(token);
+    }
+
+    @PostMapping("/refresh-token")
+    public ResponseEntity<RefreshTokenResponse> refreshToken(@RequestBody @Valid RefreshTokenRequest request) {
+        RefreshTokenResponse response = refreshTokenService.createNewAccessToken(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout() {
+        refreshTokenService.logout();
+        return ResponseEntity.ok("Logout successful");
     }
 }
 
