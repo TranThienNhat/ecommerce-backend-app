ad4e51b3c6b754973ac89da28bb4c3fd4366999f
TranThienNhat <tranthiennhat20041@gmail.com>
2025-05-10 18:49:53 +0700

    feat: add CartItemMapper with custom mapping for product info and total price

--- src/main/java/com/nhat/ecommerce_backend/service/cartitem/CartItemMapper.java
+++ src/main/java/com/nhat/ecommerce_backend/service/cartitem/CartItemMapper.java
@@ -4,6 +4,7 @@ import com.nhat.ecommerce_backend.dto.cartItem.CartItemRequest;
 import com.nhat.ecommerce_backend.dto.cartItem.CartItemResponse;
 import com.nhat.ecommerce_backend.entity.CartItem;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
@@ -11,6 +12,18 @@ import java.util.List;
 
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface CartItemMapper {
+
+    @Mapping(source = "product.id", target = "productId")
+    @Mapping(source = "product.name", target = "productName")
+    @Mapping(source = "product.imageUrl", target = "productImage")
+    @Mapping(source = "product.price", target = "price")
+    @Mapping(target = "total", expression = "java(cartItem.getProduct().getPrice().multiply(new java.math.BigDecimal(cartItem.getQuantity())))")
+    CartItemResponse toCartItemResponse(CartItem cartItem);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "cart", ignore = true)
+    @Mapping(target = "product", ignore = true)
     void updateProductFromDto(CartItemRequest request, @MappingTarget CartItem cartItem);
+
     List<CartItemResponse> toCartItemResponseList(List<CartItem> cartItems);
 }
