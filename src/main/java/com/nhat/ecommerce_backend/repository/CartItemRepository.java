e76f3dd3a74de21396b2e69184e8fe27ff877155
TranThienNhat <tranthiennhat20041@gmail.com>
2025-05-10 18:51:50 +0700

    feat: add query to fetch CartItems with products by cartId

--- src/main/java/com/nhat/ecommerce_backend/repository/CartItemRepository.java
+++ src/main/java/com/nhat/ecommerce_backend/repository/CartItemRepository.java
@@ -2,12 +2,15 @@ package com.nhat.ecommerce_backend.repository;
 
 import com.nhat.ecommerce_backend.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface CartItemRepository extends JpaRepository<CartItem, UUID> {
+
+    @Query("SELECT ci FROM CartItem ci JOIN FETCH ci.product WHERE ci.cart.id = :cartId")
     List<CartItem> findAllByCartId(UUID cartId);
 
     Optional<CartItem> findByCartIdAndProductId(UUID id, Long id1);
